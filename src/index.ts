ddcdab211a1d3cb7702738793198739f8d2033d3
Joel Campos <joelcamposoliva9000@gmail.com>
2023-09-15 17:49:44 +0200

    Fix: missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 // Index
-export * from "./hooks/use-tmwu-authentication";
 export * from "./providers/tmwu-auth.provider";
 export * from "./models/account/account.model";
+
+// Components
+export * from "./components/authenticated";
+export * from "./components/not-authenticated";
+
+// Hooks
+export * from "./hooks/use-tmwu-authentication";
+export * from "./hooks/use-tmwu-account";
