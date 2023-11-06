8c28e419cb72d3ec56758924448c77f916aaa3e5
Joel Campos <joelcamposoliva9000@gmail.com>
2023-11-06 20:57:22 +0100

    Add types and size options

--- src/utils/profile/get-profile-picture-url.ts
+++ src/utils/profile/get-profile-picture-url.ts
@@ -1,7 +1,10 @@
 import { Account } from "@tmw-universe/tmw-universe-types";
 import md5 from "blueimp-md5";
 
-export const getProfilePictureUrl = (account: Account) =>
-  `https://gravatar.com/avatar/${md5(
-    account.email.trim().toLowerCase()
-  )}?s=200`;
+export const getProfilePictureUrl = (
+  account: Account,
+  options?: { size: number }
+) =>
+  `https://gravatar.com/avatar/${md5(account.email.trim().toLowerCase())}?s=${
+    options?.size ?? 200
+  }`;
