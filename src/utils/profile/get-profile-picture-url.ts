b808dca0f6865de722cd549693444dcc53296440
Joel Campos <joelcamposoliva9000@gmail.com>
2023-11-07 16:35:00 +0100

    Make size optional

--- src/utils/profile/get-profile-picture-url.ts
+++ src/utils/profile/get-profile-picture-url.ts
@@ -3,7 +3,7 @@ import md5 from "blueimp-md5";
 
 export const getProfilePictureUrl = (
   account: Account,
-  options?: { size: number }
+  options?: { size?: number }
 ) =>
   `https://gravatar.com/avatar/${md5(account.email.trim().toLowerCase())}?s=${
     options?.size ?? 200
